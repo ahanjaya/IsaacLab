de37a7014994690c5ae2cfc6e8a4be966ce8ad29
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2025-03-30 19:57:19 +0800

    WIP: nxp_lower_body_with_torso velocity control.

--- source/isaaclab_assets/isaaclab_assets/robots/erc.py
+++ source/isaaclab_assets/isaaclab_assets/robots/erc.py
@@ -3,17 +3,12 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""Configuration for ERC robots.
-
-The following configurations are available:
-
-* :obj:`H1_CFG`: H1 humanoid robot
-"""
+"""Configuration for ERC robots."""
 
 import os
 
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import DCMotorCfg
+from isaaclab.actuators import DCMotorCfg, ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 
 ##
@@ -22,7 +17,7 @@ from isaaclab.assets.articulation import ArticulationCfg
 NXP_DIR = "source/isaaclab_assets/data/Robots/ERC/NXP"
 NXP_LOWER_BODY_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=os.path.join(os.getcwd(), NXP_DIR, "lower_body/full_collision/nxp_lower_full.usd"),
+        usd_path=os.path.join(os.getcwd(), NXP_DIR, "nxp_lower_body/nxp_lower_body.usd"),
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
@@ -34,17 +29,29 @@ NXP_LOWER_BODY_CFG = ArticulationCfg(
             max_depenetration_velocity=1.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
         ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.85),
-        joint_pos={".*": 0.0},
+        pos=(0.0, 0.0, 0.9),
+        joint_pos={
+            "left_hip_pitch_joint": 0.52,  # 30 degrees
+            "right_hip_pitch_joint": -0.52,  # -30 degrees
+            "left_hip_roll_joint": -0.05,  # -3 degrees
+            "right_hip_roll_joint": 0.05,  # 3 degrees
+            "left_hip_yaw_joint": -0.35,  # -20 degrees
+            "right_hip_yaw_joint": 0.35,  # 20 degrees
+            ".*_knee_joint": 0.79,  # 45 degrees
+            ".*_ankle_pitch_joint": -0.44,  # -25 degrees
+            ".*_ankle_roll_joint": 0.0,  # 0 degrees
+        },
         joint_vel={".*": 0.0},
     ),
-    soft_joint_pos_limit_factor=0.9,
+    soft_joint_pos_limit_factor=0.95,
     actuators={
-        "base_legs": DCMotorCfg(
+        "legs": DCMotorCfg(
             joint_names_expr=[
                 ".*_hip_pitch_joint",
                 ".*_hip_roll_joint",
@@ -65,5 +72,116 @@ NXP_LOWER_BODY_CFG = ArticulationCfg(
 
 NXP_LOWER_BODY_MINIMAL_CFG = NXP_LOWER_BODY_CFG.copy()
 NXP_LOWER_BODY_MINIMAL_CFG.spawn.usd_path = os.path.join(
-    os.getcwd(), NXP_DIR, "lower_body/minimal_collision/nxp_lower_minimal.usd"
+    os.getcwd(),
+    NXP_DIR,
+    "nxp_lower_body_minimal/nxp_lower_body_minimal.usd",
+)
+
+
+NXP_LOWER_BODY_WITH_TORSO_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=os.path.join(
+            os.getcwd(),
+            NXP_DIR,
+            "nxp_lower_body_with_torso/nxp_lower_body_with_torso.usd",
+        ),
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=4,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.85),
+        # joint_pos={".*": 0.0},
+        joint_pos={
+            "left_hip_pitch_joint": 0.52,  # 30 degrees
+            "right_hip_pitch_joint": -0.52,  # -30 degrees
+            "left_hip_roll_joint": -0.05,  # -3 degrees
+            "right_hip_roll_joint": 0.05,  # 3 degrees
+            "left_hip_yaw_joint": -0.35,  # -20 degrees
+            "right_hip_yaw_joint": 0.35,  # 20 degrees
+            ".*_knee_joint": 0.79,  # 45 degrees
+            ".*_ankle_pitch_joint": -0.44,  # -25 degrees
+            ".*_ankle_roll_joint": 0.0,  # 0 degrees
+            "torso_yaw_joint": 0.0,  # 0 degrees
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    # actuators={
+    #     "legs": DCMotorCfg(
+    #         joint_names_expr=[
+    #             ".*_hip_pitch_joint",
+    #             ".*_hip_roll_joint",
+    #             ".*_hip_yaw_joint",
+    #             ".*_knee_joint",
+    #             ".*_ankle_pitch_joint",
+    #             ".*_ankle_roll_joint",
+    #             "torso_yaw_joint",
+    #         ],
+    #         effort_limit=33.5,
+    #         saturation_effort=33.5,
+    #         velocity_limit=21.0,
+    #         stiffness=25.0,
+    #         damping=0.5,
+    #         friction=0.0,
+    #     ),
+    # },
+    actuators={
+        "legs": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_hip_yaw_joint",
+                ".*_hip_roll_joint",
+                ".*_hip_pitch_joint",
+                ".*_knee_joint",
+                "torso_yaw_joint",
+            ],
+            effort_limit=300,
+            velocity_limit=100.0,
+            stiffness={
+                ".*_hip_yaw_joint": 150.0,
+                ".*_hip_roll_joint": 150.0,
+                ".*_hip_pitch_joint": 200.0,
+                ".*_knee_joint": 200.0,
+                "torso_yaw_joint": 200.0,
+            },
+            damping={
+                ".*_hip_yaw_joint": 5.0,
+                ".*_hip_roll_joint": 5.0,
+                ".*_hip_pitch_joint": 5.0,
+                ".*_knee_joint": 5.0,
+                "torso_yaw_joint": 5.0,
+            },
+            armature={
+                ".*_hip_.*": 0.01,
+                ".*_knee_joint": 0.01,
+                "torso_yaw_joint": 0.01,
+            },
+        ),
+        "feet": ImplicitActuatorCfg(
+            effort_limit=20,
+            joint_names_expr=[".*_ankle_pitch_joint", ".*_ankle_roll_joint"],
+            stiffness=20.0,
+            damping=2.0,
+            armature=0.01,
+        ),
+    },
+)
+
+NXP_LOWER_BODY_WITH_TORSO_MINIMAL_CFG = NXP_LOWER_BODY_WITH_TORSO_CFG.copy()
+NXP_LOWER_BODY_WITH_TORSO_MINIMAL_CFG.spawn.usd_path = os.path.join(
+    os.getcwd(),
+    NXP_DIR,
+    "nxp_lower_body_with_torso_minimal/nxp_lower_body_with_torso_minimal.usd",
 )
--- source/isaaclab_tasks/isaaclab_tasks/direct/velocity_nxp/velocity_nxp_env.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/velocity_nxp/velocity_nxp_env.py
@@ -16,11 +16,7 @@ import isaaclab.utils.math as math_utils
 from isaaclab.assets import Articulation
 from isaaclab.envs import DirectRLEnv
 from isaaclab.markers import VisualizationMarkers
-from isaaclab.markers.config import (
-    BLUE_ARROW_X_MARKER_CFG,
-    GREEN_ARROW_X_MARKER_CFG,
-    RED_ARROW_X_MARKER_CFG,
-)
+from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG, GREEN_ARROW_X_MARKER_CFG, RED_ARROW_X_MARKER_CFG
 from isaaclab.sensors import ContactSensor
 from isaaclab.utils.math import quat_rotate_inverse, yaw_quat
 
@@ -30,9 +26,7 @@ from .velocity_nxp_env_cfg import VelocityNXPLowerBodyFlatEnvCfg
 class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
     cfg: VelocityNXPLowerBodyFlatEnvCfg
 
-    def __init__(
-        self, cfg: VelocityNXPLowerBodyFlatEnvCfg, render_mode: str | None = None, **kwargs
-    ):
+    def __init__(self, cfg: VelocityNXPLowerBodyFlatEnvCfg, render_mode: str | None = None, **kwargs):
         super().__init__(cfg, render_mode, **kwargs)
 
         # Joint position command (deviation from default joint positions)
@@ -69,7 +63,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
                 "termination_penalty",
                 "feet_slide",
                 "joint_deviation_hip",
-                # "joint_deviation_torso",
+                "joint_deviation_torso",
             ]
         }
         self._metric_sums = {
@@ -81,23 +75,14 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         }
 
         # Get specific body indices
-        self._base_id, _ = self._contact_sensor.find_bodies("base_link")
-        self._feet_contact_ids, _ = self._contact_sensor.find_bodies(
-            ".*_ankle_roll_link"
-        )
+        self._base_id, _ = self._contact_sensor.find_bodies("torso_link")
+        self._feet_contact_ids, _ = self._contact_sensor.find_bodies(".*_ankle_roll_link")
 
         self._feet_body_ids, _ = self._robot.find_bodies(".*_ankle_roll_link")
-        self._ankle_joint_ids, _ = self._robot.find_joints(
-            [".*_ankle_pitch_joint", ".*_ankle_roll_joint"]
-        )
-        self._hip_joint_ids, _ = self._robot.find_joints(
-            [".*_hip_yaw_joint", ".*_hip_roll_joint"]
-        )
-        # self._torso_joint_ids, _ = self._robot.find_joints("torso_joint")
-
-        self._dof_acc_torque_joint_ids, _ = self._robot.find_joints(
-            [".*_hip_.*", ".*_knee_joint"]
-        )
+        self._ankle_joint_ids, _ = self._robot.find_joints([".*_ankle_pitch_joint", ".*_ankle_roll_joint"])
+        self._hip_joint_ids, _ = self._robot.find_joints([".*_hip_yaw_joint", ".*_hip_roll_joint"])
+        self._torso_joint_ids, _ = self._robot.find_joints("torso_yaw_joint")
+        self._dof_acc_torque_joint_ids, _ = self._robot.find_joints([".*_hip_.*", ".*_knee_joint"])
 
         # add handle for debug visualization (this is set to a valid handle inside set_debug_vis)
         self.set_debug_vis(self.cfg.debug_vis)
@@ -114,7 +99,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         # clone, filter, and replicate
         self.scene.clone_environments(copy_from_source=False)
         self.scene.filter_collisions(global_prim_paths=[self.cfg.terrain.prim_path])
-        
+
         # add lights
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
         light_cfg.func("/World/Light", light_cfg)
@@ -130,9 +115,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
 
     def _pre_physics_step(self, actions: torch.Tensor):
         self._actions = actions.clone()
-        self._processed_actions = (
-            self.cfg.action_scale * self._actions + self._robot.data.default_joint_pos
-        )
+        self._processed_actions = self.cfg.action_scale * self._actions + self._robot.data.default_joint_pos
 
     def _apply_action(self):
         self._robot.set_joint_position_target(self._processed_actions)
@@ -162,7 +145,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         return observations
 
     def _get_rewards(self) -> torch.Tensor:
-        # # linear velocity tracking
+        # linear velocity tracking
         # lin_vel_error = torch.sum(torch.square(self._commands[:, :2] - self._robot.data.root_lin_vel_b[:, :2]), dim=1)
 
         # TODO: Verify this vel_yaw does it equal with the lin_vel_b?
@@ -171,34 +154,26 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
             yaw_quat(self._robot.data.root_quat_w),
             self._robot.data.root_lin_vel_w[:, :3],
         )
-        lin_vel_error = torch.sum(
-            torch.square(self._commands[:, :2] - vel_yaw[:, :2]), dim=1
-        )
+        lin_vel_error = torch.sum(torch.square(self._commands[:, :2] - vel_yaw[:, :2]), dim=1)
         lin_vel_error_mapped = torch.exp(-lin_vel_error / 0.25)
 
-        # # angular velocity tracking
+        # angular velocity tracking
         # ang_vel_error = torch.square(self._commands[:, 2] - self._robot.data.root_ang_vel_b[:, 2])
 
         # TODO: Verify this ang_vel_error does it equal with the ang_vel_b?
         # track_ang_vel_z_world_exp
-        ang_vel_error = torch.square(
-            self._commands[:, 2] - self._robot.data.root_ang_vel_w[:, 2]
-        )
+        ang_vel_error = torch.square(self._commands[:, 2] - self._robot.data.root_ang_vel_w[:, 2])
         ang_vel_error_mapped = torch.exp(-ang_vel_error / 0.25)
 
         # z velocity tracking
         z_vel_error = torch.square(self._robot.data.root_lin_vel_b[:, 2])
 
         # angular velocity x/y
-        ang_vel_error = torch.sum(
-            torch.square(self._robot.data.root_ang_vel_b[:, :2]), dim=1
-        )
+        ang_vel_error = torch.sum(torch.square(self._robot.data.root_ang_vel_b[:, :2]), dim=1)
 
         # joint torques
         joint_torques = torch.sum(
-            torch.square(
-                self._robot.data.applied_torque[:, self._dof_acc_torque_joint_ids]
-            ),
+            torch.square(self._robot.data.applied_torque[:, self._dof_acc_torque_joint_ids]),
             dim=1,
         )
 
@@ -209,31 +184,21 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         )
 
         # action rate
-        action_rate = torch.sum(
-            torch.square(self._actions - self._previous_actions), dim=1
-        )
+        action_rate = torch.sum(torch.square(self._actions - self._previous_actions), dim=1)
 
         # feet air time positive biped
         air_time = self._contact_sensor.data.current_air_time[:, self._feet_contact_ids]
-        contact_time = self._contact_sensor.data.current_contact_time[
-            :, self._feet_contact_ids
-        ]
+        contact_time = self._contact_sensor.data.current_contact_time[:, self._feet_contact_ids]
         in_contact = contact_time > 0.0
         in_mode_time = torch.where(in_contact, contact_time, air_time)
         single_stance = torch.sum(in_contact.int(), dim=1) == 1
-        air_time_reward = torch.min(
-            torch.where(single_stance.unsqueeze(-1), in_mode_time, 0.0), dim=1
-        )[0]
-        air_time_reward = torch.clamp(
-            air_time_reward, max=self.cfg.feet_air_time_threshold
-        )
+        air_time_reward = torch.min(torch.where(single_stance.unsqueeze(-1), in_mode_time, 0.0), dim=1)[0]
+        air_time_reward = torch.clamp(air_time_reward, max=self.cfg.feet_air_time_threshold)
         # no reward for zero command
         air_time_reward *= torch.norm(self._commands[:, :2], dim=1) > 0.1
 
         # flat orientation
-        flat_orientation = torch.sum(
-            torch.square(self._robot.data.projected_gravity_b[:, :2]), dim=1
-        )
+        flat_orientation = torch.sum(torch.square(self._robot.data.projected_gravity_b[:, :2]), dim=1)
 
         # dof pos limits
         # compute out of limits constraints
@@ -252,11 +217,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
 
         # feet slide
         contacts = (
-            self._contact_sensor.data.net_forces_w_history[
-                :, :, self._feet_contact_ids, :
-            ]
-            .norm(dim=-1)
-            .max(dim=1)[0]
+            self._contact_sensor.data.net_forces_w_history[:, :, self._feet_contact_ids, :].norm(dim=-1).max(dim=1)[0]
             > 1.0
         )
         body_vel = self._robot.data.body_lin_vel_w[:, self._feet_body_ids, :2]
@@ -269,52 +230,28 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         )
         joint_deviation_hip = torch.sum(torch.abs(angle), dim=1)
 
-        # # joint deviation torso
-        # angle = (
-        #     self._robot.data.joint_pos[:, self._torso_joint_ids]
-        #     - self._robot.data.default_joint_pos[:, self._torso_joint_ids]
-        # )
-        # joint_deviation_torso = torch.sum(torch.abs(angle), dim=1)
+        # joint deviation torso
+        angle = (
+            self._robot.data.joint_pos[:, self._torso_joint_ids]
+            - self._robot.data.default_joint_pos[:, self._torso_joint_ids]
+        )
+        joint_deviation_torso = torch.sum(torch.abs(angle), dim=1)
 
         rewards = {
-            "track_lin_vel_xy_exp": lin_vel_error_mapped
-            * self.cfg.track_lin_vel_xy_reward_scale
-            * self.step_dt,
-            "track_ang_vel_z_exp": ang_vel_error_mapped
-            * self.cfg.track_ang_vel_z_exp_reward_scale
-            * self.step_dt,
-            "lin_vel_z_l2": z_vel_error
-            * self.cfg.lin_vel_z_l2_reward_scale
-            * self.step_dt,
-            "ang_vel_xy_l2": ang_vel_error
-            * self.cfg.ang_vel_xy_l2_reward_scale
-            * self.step_dt,
-            "dof_torques_l2": joint_torques
-            * self.cfg.dof_torques_l2_reward_scale
-            * self.step_dt,
+            "track_lin_vel_xy_exp": lin_vel_error_mapped * self.cfg.track_lin_vel_xy_reward_scale * self.step_dt,
+            "track_ang_vel_z_exp": ang_vel_error_mapped * self.cfg.track_ang_vel_z_exp_reward_scale * self.step_dt,
+            "lin_vel_z_l2": z_vel_error * self.cfg.lin_vel_z_l2_reward_scale * self.step_dt,
+            "ang_vel_xy_l2": ang_vel_error * self.cfg.ang_vel_xy_l2_reward_scale * self.step_dt,
+            "dof_torques_l2": joint_torques * self.cfg.dof_torques_l2_reward_scale * self.step_dt,
             "dof_acc_l2": joint_accel * self.cfg.dof_acc_l2_reward_scale * self.step_dt,
-            "action_rate_l2": action_rate
-            * self.cfg.action_rate_l2_reward_scale
-            * self.step_dt,
-            "feet_air_time": air_time_reward
-            * self.cfg.feet_air_time_reward_scale
-            * self.step_dt,
-            "flat_orientation_l2": flat_orientation
-            * self.cfg.flat_orientation_l2_reward_scale
-            * self.step_dt,
-            "dof_pos_limits": out_of_limits
-            * self.cfg.dof_pos_limits_reward_scale
-            * self.step_dt,
-            "termination_penalty": is_terminated
-            * self.cfg.termination_reward_scale
-            * self.step_dt,
+            "action_rate_l2": action_rate * self.cfg.action_rate_l2_reward_scale * self.step_dt,
+            "feet_air_time": air_time_reward * self.cfg.feet_air_time_reward_scale * self.step_dt,
+            "flat_orientation_l2": flat_orientation * self.cfg.flat_orientation_l2_reward_scale * self.step_dt,
+            "dof_pos_limits": out_of_limits * self.cfg.dof_pos_limits_reward_scale * self.step_dt,
+            "termination_penalty": is_terminated * self.cfg.termination_reward_scale * self.step_dt,
             "feet_slide": feet_slide * self.cfg.feet_slide_reward_scale * self.step_dt,
-            "joint_deviation_hip": joint_deviation_hip
-            * self.cfg.joint_deviation_hip_reward_scale
-            * self.step_dt,
-            # "joint_deviation_torso": joint_deviation_torso
-            # * self.cfg.joint_deviation_torso_reward_scale
-            # * self.step_dt,
+            "joint_deviation_hip": joint_deviation_hip * self.cfg.joint_deviation_hip_reward_scale * self.step_dt,
+            "joint_deviation_torso": joint_deviation_torso * self.cfg.joint_deviation_torso_reward_scale * self.step_dt,
         }
 
         reward = torch.sum(torch.stack(list(rewards.values())), dim=0)
@@ -328,10 +265,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         time_out = self.episode_length_buf >= self.max_episode_length - 1
         net_contact_forces = self._contact_sensor.data.net_forces_w_history
         self.died = torch.any(
-            torch.max(
-                torch.norm(net_contact_forces[:, :, self._base_id], dim=-1), dim=1
-            )[0]
-            > 1.0,
+            torch.max(torch.norm(net_contact_forces[:, :, self._base_id], dim=-1), dim=1)[0] > 1.0,
             dim=1,
         )
 
@@ -345,17 +279,13 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
 
         if len(env_ids) == self.num_envs:
             # Spread out the resets to avoid spikes in training when many environments reset at a similar time
-            self.episode_length_buf[:] = torch.randint_like(
-                self.episode_length_buf, high=int(self.max_episode_length)
-            )
+            self.episode_length_buf[:] = torch.randint_like(self.episode_length_buf, high=int(self.max_episode_length))
         self._actions[env_ids] = 0.0
         self._previous_actions[env_ids] = 0.0
-        
+
         # Sample new commands
         self._update_command_metrics()
-        self._commands[env_ids] = torch.zeros_like(self._commands[env_ids]).uniform_(
-            -1.0, 1.0
-        )
+        self._commands[env_ids] = torch.zeros_like(self._commands[env_ids]).uniform_(-1.0, 1.0)
 
         # Reset robot state
         joint_pos = self._robot.data.default_joint_pos[env_ids]
@@ -370,9 +300,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         extras = dict()
         for key in self._episode_sums.keys():
             episodic_sum_avg = torch.mean(self._episode_sums[key][env_ids])
-            extras["Episode_Reward/" + key] = (
-                episodic_sum_avg / self.max_episode_length_s
-            )
+            extras["Episode_Reward/" + key] = episodic_sum_avg / self.max_episode_length_s
             self._episode_sums[key][env_ids] = 0.0
 
         self.extras["log"] = dict()
@@ -380,20 +308,14 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
 
         # Termination logging
         extras = dict()
-        extras["Episode_Termination/base_contact"] = torch.count_nonzero(
-            self.reset_terminated[env_ids]
-        ).item()
-        extras["Episode_Termination/time_out"] = torch.count_nonzero(
-            self.reset_time_outs[env_ids]
-        ).item()
+        extras["Episode_Termination/base_contact"] = torch.count_nonzero(self.reset_terminated[env_ids]).item()
+        extras["Episode_Termination/time_out"] = torch.count_nonzero(self.reset_time_outs[env_ids]).item()
         self.extras["log"].update(extras)
 
         # Command logging
         extras = dict()
         for key in self._metric_sums.keys():
-            extras["Metrics/base_velocity/" + key] = (
-                self._metric_sums[key][env_ids]
-            )
+            extras["Metrics/base_velocity/" + key] = self._metric_sums[key][env_ids]
             self._metric_sums[key][env_ids] = 0.0
 
         self.extras["log"].update(extras)
@@ -405,14 +327,10 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
 
         # logs data
         self._metric_sums["error_vel_xy"] += (
-            torch.norm(
-                self._commands[:, :2] - self._robot.data.root_lin_vel_b[:, :2], dim=-1
-            )
-            / max_command_step
+            torch.norm(self._commands[:, :2] - self._robot.data.root_lin_vel_b[:, :2], dim=-1) / max_command_step
         )
         self._metric_sums["error_vel_yaw"] += (
-            torch.abs(self._commands[:, 2] - self._robot.data.root_ang_vel_b[:, 2])
-            / max_command_step
+            torch.abs(self._commands[:, 2] - self._robot.data.root_ang_vel_b[:, 2]) / max_command_step
         )
 
     def _set_debug_vis_impl(self, debug_vis: bool):
@@ -458,19 +376,17 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
     """
 
     def _update_camera_follow_env(self):
+        if not self.cfg.camera_viewer.follow_camera:
+            return
+
         actor_pos = self._robot.data.root_pos_w[self.i_follow_env].cpu().numpy()
 
         # Smooth the camera movement with a moving average.
         new_cam_pos = actor_pos + self.follow_cam_offset
         new_cam_target = actor_pos
 
-        self.follow_cam_pos = (
-            self.k_smooth * self.follow_cam_pos + (1 - self.k_smooth) * new_cam_pos
-        )
-        self.follow_cam_target = (
-            self.k_smooth * self.follow_cam_target
-            + (1 - self.k_smooth) * new_cam_target
-        )
+        self.follow_cam_pos = self.k_smooth * self.follow_cam_pos + (1 - self.k_smooth) * new_cam_pos
+        self.follow_cam_target = self.k_smooth * self.follow_cam_target + (1 - self.k_smooth) * new_cam_target
 
         set_camera_view(self.follow_cam_pos, self.follow_cam_target)
 
@@ -479,9 +395,7 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
             return
         robot_root_pose_w = self._robot.data.root_state_w
         robot_root_pose_w[:, 2] += 0.1
-        self.base_pose_visualizer.visualize(
-            robot_root_pose_w[:, :3], robot_root_pose_w[:, 3:7]
-        )
+        self.base_pose_visualizer.visualize(robot_root_pose_w[:, :3], robot_root_pose_w[:, 3:7])
 
         # get marker location
         # -- base state
@@ -489,34 +403,27 @@ class VelocityNXPLowerBodyFlatEnv(DirectRLEnv):
         base_pos_w[:, 2] += 0.5
 
         # -- resolve the scales and quaternions
-        vel_des_arrow_scale, vel_des_arrow_quat = self._resolve_xy_velocity_to_arrow(
-            self._commands[:, :2]
-        )
-        vel_arrow_scale, vel_arrow_quat = self._resolve_xy_velocity_to_arrow(
-            self._robot.data.root_lin_vel_b[:, :2]
-        )
+        vel_des_arrow_scale, vel_des_arrow_quat = self._resolve_xy_velocity_to_arrow(self._commands[:, :2])
+        vel_arrow_scale, vel_arrow_quat = self._resolve_xy_velocity_to_arrow(self._robot.data.root_lin_vel_b[:, :2])
 
         # display markers
-        self.base_vel_goal_visualizer.visualize(
-            base_pos_w, vel_des_arrow_quat, vel_des_arrow_scale
-        )
+        self.base_vel_goal_visualizer.visualize(base_pos_w, vel_des_arrow_quat, vel_des_arrow_scale)
         self.base_vel_visualizer.visualize(base_pos_w, vel_arrow_quat, vel_arrow_scale)
 
-    def _resolve_xy_velocity_to_arrow(
-        self, xy_velocity: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor]:
+    def _resolve_xy_velocity_to_arrow(self, xy_velocity: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         """Converts the XY base velocity command to arrow direction rotation."""
         # obtain default scale of the marker
         default_scale = self.base_vel_goal_visualizer.cfg.markers["arrow"].scale
+
         # arrow-scale
-        arrow_scale = torch.tensor(default_scale, device=self.device).repeat(
-            xy_velocity.shape[0], 1
-        )
+        arrow_scale = torch.tensor(default_scale, device=self.device).repeat(xy_velocity.shape[0], 1)
         arrow_scale[:, 0] *= torch.linalg.norm(xy_velocity, dim=1) * 3.0
+
         # arrow-direction
         heading_angle = torch.atan2(xy_velocity[:, 1], xy_velocity[:, 0])
         zeros = torch.zeros_like(heading_angle)
         arrow_quat = math_utils.quat_from_euler_xyz(zeros, zeros, heading_angle)
+
         # convert everything back from base to world frame
         base_quat_w = self._robot.data.root_quat_w
         arrow_quat = math_utils.quat_mul(base_quat_w, arrow_quat)
