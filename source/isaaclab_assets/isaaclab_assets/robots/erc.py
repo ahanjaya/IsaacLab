bcc74f7cb8638767280ed084d007aa6784207d70
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2025-04-07 22:45:35 +0800

    Adding hip_yaw collision and enable self_collision.

--- source/isaaclab_assets/isaaclab_assets/robots/erc.py
+++ source/isaaclab_assets/isaaclab_assets/robots/erc.py
@@ -113,7 +113,7 @@ NXP_LOWER_BODY_WITH_TORSO_CFG = ArticulationCfg(
             max_depenetration_velocity=1.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False,
+            enabled_self_collisions=True,
             solver_position_iteration_count=8,
             solver_velocity_iteration_count=4,
         ),
