f93a20df06826819271edf4e93cbb3a058379eeb
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2025-04-12 15:01:24 +0800

    Adding nxp lower body direct task in rough terrain.

--- source/isaaclab_tasks/isaaclab_tasks/direct/velocity_nxp/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/velocity_nxp/__init__.py
@@ -24,3 +24,13 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:VelocityNXPLowerBodyFlatPPORunnerCfg",
     },
 )
+
+gym.register(
+    id="Isaac-Velocity-Rough-NXP-Lower-Body-Direct-v0",
+    entry_point=f"{__name__}.velocity_nxp_env:VelocityNXPLowerBodyFlatEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.velocity_nxp_env:VelocityNXPLowerBodyRoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:VelocityNXPLowerBodyRoughPPORunnerCfg",
+    },
+)
