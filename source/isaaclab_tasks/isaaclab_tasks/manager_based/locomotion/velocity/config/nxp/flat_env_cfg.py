fa38e238a719d3c769c1eca68095e3759dc4325a
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2025-04-14 23:16:37 +0800

    Adding DR into nxp fla_env_cfg.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/flat_env_cfg.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils import configclass
 
 from .rough_env_cfg import NXPRoughEnvCfg
@@ -24,31 +23,10 @@ class NXPFlatEnvCfg(NXPRoughEnvCfg):
         # no terrain curriculum
         self.curriculum.terrain_levels = None
 
-        # Rewards inspired by isaac berkeley
-        self.rewards.track_lin_vel_xy_exp.weight = 1.0
-        self.rewards.track_ang_vel_z_exp.weight = 0.5
-        self.rewards.lin_vel_z_l2.weight = -2.0
-        self.rewards.ang_vel_xy_l2.weight = -0.05
-        self.rewards.dof_acc_l2 = None
+        # Rewards
         self.rewards.dof_torques_l2.weight = -2.5e-5
-        self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg("robot", joint_names=[".*_joint"])
-        self.rewards.action_rate_l2.weight = -0.01
         self.rewards.feet_air_time.weight = 0.5
-        self.rewards.feet_slide.weight = -0.25
-        self.rewards.undesired_contacts.weight = -1.0
-        self.rewards.undesired_contacts.params["sensor_cfg"] = SceneEntityCfg(
-            "contact_forces", body_names=".*_hip_yaw_link"
-        )
-        self.rewards.joint_deviation_hip.weight = -0.1
-        self.rewards.joint_deviation_knee.weight = -0.01
-        self.rewards.joint_deviation_torso.weight = -1.0
         self.rewards.flat_orientation_l2.weight = -5.0
-        self.rewards.dof_pos_limits.weight = -1.0
-
-        # Commands
-        self.commands.base_velocity.ranges.lin_vel_x = (-1.0, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
-        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
 
 class NXPFlatEnvCfg_PLAY(NXPFlatEnvCfg):
@@ -64,3 +42,5 @@ class NXPFlatEnvCfg_PLAY(NXPFlatEnvCfg):
         # remove random pushing
         self.events.base_external_force_torque = None
         self.events.push_robot = None
+        # commands
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 0.75)
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/rough_env_cfg.py
@@ -84,8 +84,16 @@ class NXPRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/torso_link"
 
         # Randomization
-        self.events.push_robot = None
-        self.events.add_base_mass = None
+        self.events.push_robot.params["velocity_range"] = {
+            "x": (-0.5, 0.5),
+            "y": (-0.5, 0.5),
+            "z": (-0.5, 0.5),
+            "roll": (-0.5, 0.5),
+            "pitch": (-0.5, 0.5),
+            "yaw": (-0.5, 0.5),
+        }
+        self.events.add_base_mass.params["asset_cfg"].body_names = ["torso_link"]
+        self.events.add_base_mass.params["mass_distribution_params"] = (-3.0, 3.0)
         self.events.reset_robot_joints.params["position_range"] = (1.0, 1.0)
         self.events.base_external_force_torque.params["asset_cfg"].body_names = ["torso_link"]
         self.events.reset_base.params = {
@@ -100,22 +108,30 @@ class NXPRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
             },
         }
 
-        # Rewards
-        self.rewards.lin_vel_z_l2.weight = 0.0
-        self.rewards.flat_orientation_l2.weight = -1.0
-        self.rewards.action_rate_l2.weight = -0.005
-        self.rewards.dof_acc_l2.weight = -1.25e-7
-        self.rewards.dof_acc_l2.params["asset_cfg"] = SceneEntityCfg(
-            "robot", joint_names=[".*_hip_.*", ".*_knee_joint"]
+        # Rewards inspired by isaac berkeley
+        self.rewards.track_lin_vel_xy_exp.weight = 1.0
+        self.rewards.track_ang_vel_z_exp.weight = 0.5
+        self.rewards.lin_vel_z_l2.weight = -2.0
+        self.rewards.ang_vel_xy_l2.weight = -0.05
+        self.rewards.dof_pos_limits.weight = -1.0
+        self.rewards.dof_acc_l2 = None
+        self.rewards.dof_torques_l2.weight = -1.0e-5
+        self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg("robot", joint_names=[".*_joint"])
+        self.rewards.action_rate_l2.weight = -0.01
+        self.rewards.feet_air_time.weight = 2.0
+        self.rewards.feet_slide.weight = -0.25
+        self.rewards.undesired_contacts.weight = -1.0
+        self.rewards.undesired_contacts.params["sensor_cfg"] = SceneEntityCfg(
+            "contact_forces", body_names=".*_hip_yaw_link"
         )
-        self.rewards.dof_torques_l2.weight = -1.5e-7
-        self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg(
-            "robot", joint_names=[".*_hip_.*", ".*_knee_joint", ".*_ankle_.*"]
-        )
+        self.rewards.joint_deviation_hip.weight = -0.1
+        self.rewards.joint_deviation_knee.weight = -0.01
+        self.rewards.joint_deviation_torso.weight = -1.0
+        self.rewards.flat_orientation_l2.weight = -0.5
 
         # Commands
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (-0.0, 0.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
         # terminations
@@ -140,8 +156,8 @@ class NXPRoughEnvCfg_PLAY(NXPRoughEnvCfg):
             self.scene.terrain.terrain_generator.num_cols = 5
             self.scene.terrain.terrain_generator.curriculum = False
 
-        self.commands.base_velocity.ranges.lin_vel_x = (1.0, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
         self.commands.base_velocity.ranges.heading = (0.0, 0.0)
         # disable randomization for play
