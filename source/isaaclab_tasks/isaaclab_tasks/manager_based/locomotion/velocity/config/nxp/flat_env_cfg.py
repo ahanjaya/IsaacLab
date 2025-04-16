38b879af3071c813c5f4d599f3191e6a446ce65e
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2025-04-16 08:21:44 +0800

    Tuning nxp rough terrain reward function and cancel push and base mass.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/flat_env_cfg.py
@@ -17,16 +17,46 @@ class NXPFlatEnvCfg(NXPRoughEnvCfg):
         # change terrain to flat
         self.scene.terrain.terrain_type = "plane"
         self.scene.terrain.terrain_generator = None
+
         # no height scan
         self.scene.height_scanner = None
         self.observations.policy.height_scan = None
+
         # no terrain curriculum
         self.curriculum.terrain_levels = None
 
+        # Randomization
+        self.events.add_base_mass.params["mass_distribution_params"] = (-3.0, 3.0)
+        self.events.reset_base.params = {
+            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
+            "velocity_range": {
+                "x": (-0.1, 0.1),
+                "y": (-0.1, 0.1),
+                "z": (-0.1, 0.1),
+                "roll": (-0.1, 0.1),
+                "pitch": (-0.1, 0.1),
+                "yaw": (-0.1, 0.1),
+            },
+        }
+        self.events.push_robot.params["velocity_range"] = {
+            "x": (-0.5, 0.5),
+            "y": (-0.5, 0.5),
+            "z": (-0.5, 0.5),
+            "roll": (-0.5, 0.5),
+            "pitch": (-0.5, 0.5),
+            "yaw": (-0.5, 0.5),
+        }
+
         # Rewards
+        self.rewards.lin_vel_z_l2.weight = -2.0
         self.rewards.dof_torques_l2.weight = -2.5e-5
         self.rewards.feet_air_time.weight = 0.5
         self.rewards.flat_orientation_l2.weight = -5.0
+
+        # Commands
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
+        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
 
 class NXPFlatEnvCfg_PLAY(NXPFlatEnvCfg):
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/nxp/rough_env_cfg.py
@@ -85,15 +85,15 @@ class NXPRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
 
         # Randomization
         self.events.push_robot.params["velocity_range"] = {
-            "x": (-0.5, 0.5),
-            "y": (-0.5, 0.5),
-            "z": (-0.5, 0.5),
-            "roll": (-0.5, 0.5),
-            "pitch": (-0.5, 0.5),
-            "yaw": (-0.5, 0.5),
+            "x": (-0.0, 0.0),
+            "y": (-0.0, 0.0),
+            "z": (-0.0, 0.0),
+            "roll": (-0.0, 0.0),
+            "pitch": (-0.0, 0.0),
+            "yaw": (-0.0, 0.0),
         }
         self.events.add_base_mass.params["asset_cfg"].body_names = ["torso_link"]
-        self.events.add_base_mass.params["mass_distribution_params"] = (-3.0, 3.0)
+        self.events.add_base_mass.params["mass_distribution_params"] = (-0.0, 0.0)
         self.events.reset_robot_joints.params["position_range"] = (1.0, 1.0)
         self.events.base_external_force_torque.params["asset_cfg"].body_names = ["torso_link"]
         self.events.reset_base.params = {
@@ -115,10 +115,10 @@ class NXPRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.ang_vel_xy_l2.weight = -0.05
         self.rewards.dof_pos_limits.weight = -1.0
         self.rewards.dof_acc_l2 = None
-        self.rewards.dof_torques_l2.weight = -1.0e-5
+        self.rewards.dof_torques_l2.weight = -2.5e-5
         self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg("robot", joint_names=[".*_joint"])
         self.rewards.action_rate_l2.weight = -0.01
-        self.rewards.feet_air_time.weight = 2.0
+        self.rewards.feet_air_time.weight = 0.5
         self.rewards.feet_slide.weight = -0.25
         self.rewards.undesired_contacts.weight = -1.0
         self.rewards.undesired_contacts.params["sensor_cfg"] = SceneEntityCfg(
@@ -127,10 +127,10 @@ class NXPRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.joint_deviation_hip.weight = -0.1
         self.rewards.joint_deviation_knee.weight = -0.01
         self.rewards.joint_deviation_torso.weight = -1.0
-        self.rewards.flat_orientation_l2.weight = -0.5
+        self.rewards.flat_orientation_l2.weight = -5.0
 
         # Commands
-        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.0, 1.0)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
@@ -156,7 +156,7 @@ class NXPRoughEnvCfg_PLAY(NXPRoughEnvCfg):
             self.scene.terrain.terrain_generator.num_cols = 5
             self.scene.terrain.terrain_generator.curriculum = False
 
-        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.0, 0.75)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
         self.commands.base_velocity.ranges.heading = (0.0, 0.0)
