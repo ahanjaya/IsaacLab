5af57d1a4a7499c3a0247539b7908b727a88c691
Hanjaya Hsu <hsu.hanjaya@inventec.com>
2026-02-04 13:50:51 +0800

    Adding RBA05 files.

--- source/isaaclab_assets/isaaclab_assets/robots/inventec.py
+++ source/isaaclab_assets/isaaclab_assets/robots/inventec.py
@@ -21,6 +21,37 @@ from isaaclab.assets.articulation import ArticulationCfg
 INVENTEC_DIR = "source/isaaclab_assets/data/Robots/Inventec"
 
 
+RBA05_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=os.path.join(os.getcwd(), INVENTEC_DIR, "rba05/rba05.usd"),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "joint1": 0.0,
+            "joint2": 0.0,
+            "joint3": 0.0,
+            "joint4": 0.0,
+            "joint5": 0.0,
+            "joint6": 0.0,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit_sim=100.0,
+            effort_limit_sim=87.0,
+            stiffness=800.0,
+            damping=40.0,
+        ),
+    },
+)
+"""Configuration of RBA05 arm using implicit actuator models."""
+
 RBA10_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path=os.path.join(os.getcwd(), INVENTEC_DIR, "rba10/rba10.usd"),
