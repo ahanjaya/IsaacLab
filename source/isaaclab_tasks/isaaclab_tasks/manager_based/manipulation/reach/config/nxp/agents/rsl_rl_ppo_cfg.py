63973887f5c3e346a4946048636246da456b0da2
Hsu.Hanjaya <ahanjaya@gmail.com>
2026-07-22 13:53:42 +0800

    Adding left_arm in NXPReach task.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/config/nxp/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/config/nxp/agents/rsl_rl_ppo_cfg.py
@@ -18,9 +18,9 @@ class NXPReachPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     resume = False
     empirical_normalization = True
     policy = RslRlPpoActorCriticCfg(
-        init_noise_std=1.0,
-        actor_hidden_dims=[64, 64],
-        critic_hidden_dims=[64, 64],
+        init_noise_std=1.5,
+        actor_hidden_dims=[128, 128, 128],
+        critic_hidden_dims=[128, 128, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
